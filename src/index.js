bc3d7e62a4489099b6948cd9e3a162195bfb656d
Tumelo Ceba <tumeloceba@icloud.com>
2022-02-21 19:04:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+////119 vulnerabilities (101 moderate, 16 high, 2 critical)
